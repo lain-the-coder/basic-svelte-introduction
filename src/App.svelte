39004a290778583fca3f8ac0f765da4516fbb6d0
Ehab Ahmed (lain-the-coder) <ahmed.ehabj@gmail.com>
2026-08-19 18:29:00 +0400

    practiced - curly braces, script tag, data, html tags, nested components

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,9 @@
-<h1>lain-the-svelter</h1>
+<script>
+  import Nested from "./Nested.svelte";
+  let name = "lain";
+  let string = `this is body with some <strong>HTML!!!</strong>`;
+</script>
+
+<h1>Welcome, {name.toUpperCase()}</h1>
+<Nested></Nested>
+<p>{@html string}</p>
---
+++ src/Nested.svelte
@@ -0,0 +1,7 @@
+<script>
+  let age = 26;
+  let gender = "male";
+</script>
+
+<h2>Age: {age}</h2>
+<h2>Gender: {gender}</h2>
