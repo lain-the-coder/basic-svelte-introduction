fc39ddd1c8253345c4732d667a352345b2305e57
Ehab Ahmed (lain-the-coder) <ahmed.ehabj@gmail.com>
2026-08-19 18:40:07 +0400

    practiced - runes/state which essentially is a variable that when updated in an application (like js function) is also updated in DOM real-time; done by declaring a variable with (<initial-value>)

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,15 @@
   import Nested from "./Nested.svelte";
   let name = "lain";
   let string = `this is body with some <strong>HTML!!!</strong>`;
+  let count = $state(0);
+  function increment() {
+    count += 1;
+  }
 </script>
 
 <h1>Welcome, {name.toUpperCase()}</h1>
 <Nested></Nested>
 <p>{@html string}</p>
+<button onclick={increment}>
+  Clicks: {count}
+</button>
