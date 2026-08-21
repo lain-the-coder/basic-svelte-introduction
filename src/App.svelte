e8b78c11c29e855adf828997d0f840f656a4218c
Ehab Ahmed (lain-the-coder) <ahmed.ehabj@gmail.com>
2026-08-22 02:53:53 +0400

    practiced - two use cases for effects; mount once effect use mirroring fetchintialdata(); timer with effect that will be used to measure idle time

--- src/App.svelte
+++ src/App.svelte
@@ -7,12 +7,22 @@
   let price = $state(5);
   let quantity = $state(10);
   let total = $derived(price * quantity);
+  let secondsActive = $state(0);
   $inspect(items).with((type, value) => {
     console.log("items updated to: ", value);
   });
+  // mount once effect use
+  $effect(() => console.log("im learning effects!!"));
   function increment() {
     count += 1;
   }
+  // timer with effect
+  $effect(() => {
+    const timer = setInterval(() => (secondsActive += 1), 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  });
   function mutation() {
     items.push("Cherry");
     items[1] = "Avacado";
@@ -26,6 +36,7 @@
 <h1>Welcome, {name.toUpperCase()}</h1>
 <Nested></Nested>
 <p>{@html string}</p>
+<p>Active Session: {secondsActive}</p>
 <button onclick={increment}>
   Button Clicks: {count}
 </button>
